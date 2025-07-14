555ee541aa9002489798c76669a740d14cf14cf7
Michael Chukwu <naemugo@gmail.com>
2025-07-14 09:24:58 +0100

    added version and created versions for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post, Version } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -6,12 +6,15 @@ import { AuthDto } from "./dto";
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @HttpCode(HttpStatus.CREATED)
+    @Version('1')
     @Post('signup')
     signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto)
     }
 
     @HttpCode(HttpStatus.OK)
+    @Version('1')
     @Post('login')
     login(@Body() dto: AuthDto) {
         return this.authService.login(dto)
